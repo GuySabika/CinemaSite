503b4fd4dad4294addb25148292e1bacf2fe9c09
GuySabika <guysabika@gmail.com>
2025-01-28 11:55:02 +0200

    working on login and signup connection

--- UserCinema/src/feildData.js
+++ UserCinema/src/feildData.js
@@ -1,6 +1,6 @@
 export const feildData = {
     loging: { feildsInput: [{ name: "Email", type: "email" }, { name: "Password", type: "password" }], fieldSelect: [], isMultiple: false },
-    signup: { feildsInput: [{ name: "Username", type: "text" }, { name: "Password", type: "password" }, { name: "Email", type: "email" }], fieldSelect: [], isMultiple: false }
+    signup: { feildsInput: [{ name: "Name", type: "text" }, { name: "Password", type: "password" }, { name: "Email", type: "email" }], fieldSelect: [], isMultiple: false }
 };
 export const elementData = {
     movie: { show: ["Actor", "AgeRestriction", "ReleaseDate"] },
